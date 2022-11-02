ff0a5eedb9869899cd895c2ab80e2671dbc29d4c
andreymlv <ndrmlv@ya.ru>
2022-11-02 16:03:22 +0300

    Add move mechanic.

--- CourseWork/src/src/game.py
+++ CourseWork/src/src/game.py
@@ -1,10 +1,13 @@
-from typing import NamedTuple
+from dataclasses import dataclass
 import pygame
 
+from disk import Disk
+from drawable import Drawable
 from tower import Tower
 
 
-class Game(NamedTuple):
+@dataclass()
+class Game(Drawable):
     towers: list[Tower]
     clock: pygame.time.Clock
     window: pygame.surface.Surface
@@ -23,3 +26,23 @@ class Game(NamedTuple):
         self.window.fill((255, 255, 255))
         pygame.display.update()
         self.clock.tick(60)
+
+    def move(self, from_tower: int, to_tower: int):
+        if self.can_move(from_tower, to_tower):
+            towers: list[Tower] = self.towers.copy()
+            temp_disk: Disk = towers[from_tower].last()
+            towers[from_tower] = towers[from_tower].pop()
+            towers[to_tower] = towers[to_tower].push(temp_disk)
+            return Game(towers, self.clock, self.window, self.over)
+        return self
+
+    def can_move(self, from_tower: int, to_tower: int) -> bool:
+        return self.towers[from_tower].can_pop() and self.towers[to_tower].can_push(
+            self.towers[from_tower].last()
+        )
+
+    def draw(self) -> pygame.Rect:
+        return super().draw()
+
+    def print(self) -> str:
+        return super().print()
